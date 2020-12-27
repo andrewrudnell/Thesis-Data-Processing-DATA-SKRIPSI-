004c00e417b4730ab0e9f31b5768cd83655ce4b2
andrewrudnell <andrew16001@mail.unpad.ac.id>
2020-12-27 18:18:03 +0700

    init

---
+++ Coba.py
@@ -0,0 +1,6 @@
+num= int(input('masukan bilangan:'))   
+if num > 1 :  
+     for i in range ( 2, num ):
+         print(i)
+x= (i%num)
+print(x)
---
+++ Main.py
@@ -0,0 +1,25 @@
+import time
+start_time = time.time()
+print("Halo Dunia!!!!!!")
+print ("aselemele")
+a= 10
+print(a)
+
+print(time.time() - start_time, "detik")
+#kita bisa mengcompile data
+#namanya bytecode
+# cara mengcompile, buka terminal dan tuliskan
+# python -m py_compile Main.py
+
+
+
+a= 10
+x= 5
+panjang = 1000
+print("Nilai a=", a)
+
+z= 3456%1000
+print(z)
+g= 2
+j=g*2
+j
